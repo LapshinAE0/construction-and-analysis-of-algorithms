3d7bff96d718aff8c2d8bb972e94aef4a7ef5947
LapshinAE0 <sasha_lapshin_2006@mail.ru>
2026-04-21 11:56:13 +0300

    fix: code stepik

--- Lapshin_Aleksandr_lb3/stepik-4-2.cpp
+++ Lapshin_Aleksandr_lb3/stepik-4-2.cpp
@@ -7,37 +7,38 @@ using namespace std;
 
 
 int main() {
-    int cost_replace, cost_insert, cost_delete;
-    cin >> cost_replace >> cost_insert >> cost_delete;
-    
     std::string S, T;
     std::cin >> S >> T;
     
-    int n = S.length();
-    int m = T.length();
+    int len_S = S.length();
+    int len_T = T.length();
     
-    std::vector<std::vector<int>> matrix(n + 1, std::vector<int>(m + 1, 0));
+    std::vector<std::vector<int>> matrix(len_S + 1, std::vector<int>(len_T + 1, 0));
     
-    for (int i = 0; i <= n; i++) {
+    for (int i = 0; i <= len_S; i++) {
         matrix[i][0] = i;
     }
-    for (int j = 0; j <= m; j++) {
-        matrix[0][j] = j;
+    for (int i = 0; i <= len_T; i++) {
+        matrix[0][i] = i;
     }
     
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
+    for (int i = 1; i <= len_S; i++) {
+        for (int j = 1; j <= len_T; j++) {
             if (S[i - 1] == T[j - 1]) {
                 matrix[i][j] = matrix[i - 1][j - 1];
             }
             else {
-                matrix[i][j] = min({matrix[i - 1][j] + 1, matrix[i][j - 1] + 1, matrix[i - 1][j - 1] + 1});
+                int del_cost = matrix[i - 1][j] + 1;
+                int ins_cost = matrix[i][j - 1] + 1;
+                int rep_cost = matrix[i - 1][j - 1] + 1;
+                matrix[i][j] = min({del_cost, ins_cost, rep_cost});
             }
         }
     }
     
-    std::cout << matrix[n][m] << std::endl;
+    std::cout << matrix[len_S][len_T] << std::endl;
     
     return 0;
 }
+
 
